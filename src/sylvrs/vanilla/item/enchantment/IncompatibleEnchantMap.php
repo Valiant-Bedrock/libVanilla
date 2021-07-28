e7677fee2ab5f639c5b09ae63db60b6f3d10eefb
Em Jordan <sylvrs@proton.me>
2021-07-28 18:07:03 -0500

    IncompatibleEnchantMap: Fix mapping issue

--- src/sylvrs/vanilla/item/enchantment/IncompatibleEnchantMap.php
+++ src/sylvrs/vanilla/item/enchantment/IncompatibleEnchantMap.php
@@ -14,48 +14,50 @@ final class IncompatibleEnchantMap {
 	 * I hate this... but it works, for now
 	 */
 	public const MAP = [
-		EnchantmentIds::PROTECTION => [EnchantmentIds::FIRE_PROTECTION, EnchantmentIds::BLAST_PROTECTION, EnchantmentIds::PROJECTILE_PROTECTION],
-		EnchantmentIds::FIRE_PROTECTION => [EnchantmentIds::PROTECTION, EnchantmentIds::BLAST_PROTECTION, EnchantmentIds::PROJECTILE_PROTECTION],
+		EnchantmentIds::PROTECTION => [EnchantmentIds::FIRE_PROTECTION => true, EnchantmentIds::BLAST_PROTECTION => true, EnchantmentIds::PROJECTILE_PROTECTION => true],
+		EnchantmentIds::FIRE_PROTECTION => [EnchantmentIds::PROTECTION => true, EnchantmentIds::BLAST_PROTECTION => true, EnchantmentIds::PROJECTILE_PROTECTION => true],
 		//EnchantmentIds::FEATHER_FALLING => [],
-		EnchantmentIds::BLAST_PROTECTION => [EnchantmentIds::PROTECTION, EnchantmentIds::FIRE_PROTECTION, EnchantmentIds::PROJECTILE_PROTECTION],
-		EnchantmentIds::PROJECTILE_PROTECTION => [EnchantmentIds::PROTECTION, EnchantmentIds::FIRE_PROTECTION, EnchantmentIds::BLAST_PROTECTION],
+		EnchantmentIds::BLAST_PROTECTION => [EnchantmentIds::PROTECTION => true, EnchantmentIds::FIRE_PROTECTION => true, EnchantmentIds::PROJECTILE_PROTECTION => true],
+		EnchantmentIds::PROJECTILE_PROTECTION => [EnchantmentIds::PROTECTION => true, EnchantmentIds::FIRE_PROTECTION => true, EnchantmentIds::BLAST_PROTECTION => true],
 		//EnchantmentIds::THORNS => [],
 		//EnchantmentIds::RESPIRATION => [],
-		EnchantmentIds::DEPTH_STRIDER => [EnchantmentIds::FROST_WALKER],
+		EnchantmentIds::DEPTH_STRIDER => [EnchantmentIds::FROST_WALKER => true],
 		//EnchantmentIds::AQUA_AFFINITY => [],
-		EnchantmentIds::SHARPNESS => [EnchantmentIds::SMITE, EnchantmentIds::BANE_OF_ARTHROPODS],
-		EnchantmentIds::SMITE => [EnchantmentIds::SHARPNESS, EnchantmentIds::BANE_OF_ARTHROPODS],
-		EnchantmentIds::BANE_OF_ARTHROPODS => [EnchantmentIds::SHARPNESS, EnchantmentIds::SMITE],
+		EnchantmentIds::SHARPNESS => [EnchantmentIds::SMITE => true, EnchantmentIds::BANE_OF_ARTHROPODS => true],
+		EnchantmentIds::SMITE => [EnchantmentIds::SHARPNESS => true, EnchantmentIds::BANE_OF_ARTHROPODS => true],
+		EnchantmentIds::BANE_OF_ARTHROPODS => [EnchantmentIds::SHARPNESS => true, EnchantmentIds::SMITE => true],
 		//EnchantmentIds::KNOCKBACK => [],
 		//EnchantmentIds::FIRE_ASPECT => [],
-		EnchantmentIds::LOOTING => [EnchantmentIds::SILK_TOUCH],
+		EnchantmentIds::LOOTING => [EnchantmentIds::SILK_TOUCH => true],
 		//EnchantmentIds::EFFICIENCY => [],
-		EnchantmentIds::SILK_TOUCH => [EnchantmentIds::FORTUNE, EnchantmentIds::LOOTING, EnchantmentIds::LUCK_OF_THE_SEA],
+		EnchantmentIds::SILK_TOUCH => [EnchantmentIds::FORTUNE => true, EnchantmentIds::LOOTING => true, EnchantmentIds::LUCK_OF_THE_SEA => true],
 		//EnchantmentIds::UNBREAKING => [],
-		EnchantmentIds::FORTUNE => [EnchantmentIds::SILK_TOUCH],
+		EnchantmentIds::FORTUNE => [EnchantmentIds::SILK_TOUCH => true],
 		//EnchantmentIds::POWER => [],
 		//EnchantmentIds::PUNCH => [],
 		//EnchantmentIds::FLAME => [],
-		EnchantmentIds::INFINITY => [EnchantmentIds::MENDING],
-		EnchantmentIds::LUCK_OF_THE_SEA => [EnchantmentIds::SILK_TOUCH],
+		EnchantmentIds::INFINITY => [EnchantmentIds::MENDING => true],
+		EnchantmentIds::LUCK_OF_THE_SEA => [EnchantmentIds::SILK_TOUCH => true],
 		//EnchantmentIds::LURE => [],
-		EnchantmentIds::FROST_WALKER => [EnchantmentIds::DEPTH_STRIDER],
-		EnchantmentIds::MENDING => [EnchantmentIds::INFINITY],
+		EnchantmentIds::FROST_WALKER => [EnchantmentIds::DEPTH_STRIDER => true],
+		EnchantmentIds::MENDING => [EnchantmentIds::INFINITY => true],
 		//EnchantmentIds::BINDING => [],
 		//EnchantmentIds::VANISHING => [],
 		//EnchantmentIds::IMPALING => [],
-		EnchantmentIds::RIPTIDE => [EnchantmentIds::LOYALTY, EnchantmentIds::CHANNELING],
-		EnchantmentIds::LOYALTY => [EnchantmentIds::RIPTIDE],
-		EnchantmentIds::CHANNELING => [EnchantmentIds::RIPTIDE],
-		EnchantmentIds::MULTISHOT => [EnchantmentIds::PIERCING],
-		EnchantmentIds::PIERCING => [EnchantmentIds::MULTISHOT],
+		EnchantmentIds::RIPTIDE => [EnchantmentIds::LOYALTY => true, EnchantmentIds::CHANNELING => true],
+		EnchantmentIds::LOYALTY => [EnchantmentIds::RIPTIDE => true],
+		EnchantmentIds::CHANNELING => [EnchantmentIds::RIPTIDE => true],
+		EnchantmentIds::MULTISHOT => [EnchantmentIds::PIERCING => true],
+		EnchantmentIds::PIERCING => [EnchantmentIds::MULTISHOT => true],
 		//EnchantmentIds::QUICK_CHARGE => [],
 		//EnchantmentIds::SOUL_SPEED => []
 	];
 
 	public static function isIncompatible(Enchantment $first, Enchantment $second): bool {
 		$map = EnchantmentIdMap::getInstance();
-		return isset(self::MAP[$map->toId($first)][$map->toId($second)]);
+		$firstId = $map->toId($first);
+		$secondId = $map->toId($second);
+		return isset(self::MAP[$firstId][$secondId]) || isset(self::MAP[$secondId][$firstId]);
 	}
 
 }
